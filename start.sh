9a17f7df721e8585d414edc0035b4e71843ea76e
kbvstmd <kbvstmd@gmail.com>
2022-04-06 21:41:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-wget -c http://118.31.70.55/ProGro-neo/data/test.tar.gz
-wget -c http://118.31.70.55/ProGro-neo/data/soft.tar.gz
-wget -c http://118.31.70.55/ProGro-neo/data/reference_files.tar.gz
+wget -c http://118.31.70.55/ProGeo-neo/data/test.tar.gz
+wget -c http://118.31.70.55/ProGeo-neo/data/soft.tar.gz
+wget -c http://118.31.70.55/ProGeo-neo/data/reference_files.tar.gz
 tar -zxvf *.gz
